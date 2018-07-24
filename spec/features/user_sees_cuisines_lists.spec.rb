da4c805e2b541b9e215950839758aa9e271cae32
Eunice Choi <eunicechoistudio@gmail.com>
2018-07-24 14:14:20 -0400

    Feature 1 upddated: Feature testing updated

--- spec/features/user_sees_cuisines_lists.spec.rb
+++ spec/features/user_sees_cuisines_lists.spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 feature "user sees a list of cuisines" do
-  scenario "see all cuisines" do
-    cuisine1 = Cuisine.create(name: "Chinese")
-    cuisine2 = Cuisine.create(name: "American")
-    cuisine3 = Cuisine.create(name: "Italian")
+  let!(:cuisine1) {Cuisine.create(name: "Chinese")}
+  let!(:cuisine2) {Cuisine.create(name: "American")}
+  let!(:cuisine3) {Cuisine.create(name: "Italian")}
 
+  scenario "see all cuisines" do
     visit cuisines_path
 
     expect(page).to have_content("Welcome to Effective Fork")
