90faec0d89e2d76ac42c969087db9071ebe6da75
GK <gk@gk.local>
2018-08-02 20:57:16 -0400

    removed binding.pry from restaurants_controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -29,7 +29,6 @@ class Api::V1::RestaurantsController < ApplicationController
     restaurant_photo = RestaurantPhoto.new
     restaurant_photo.restaurant_photo = params[:photo]
     restaurant_photo.user = current_user
-    binding.pry
     restaurant_photo
   end
 
